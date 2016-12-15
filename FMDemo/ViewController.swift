f082c3c8ba8ae35c990b3da6dc8140d10008e0e5
ScarletSmallRed <137452596@qq.com>
2016-12-15 13:25:40 +0800

    Version 3.0

--- FMDemo/ViewController.swift
+++ FMDemo/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var iv: EkoImage!
     @IBOutlet weak var bg: EkoImage!
+    @IBOutlet weak var tv: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +29,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tv.dequeueReusableCell(withIdentifier: "SongCell")! as UITableViewCell
+        
+        cell.textLabel?.text = NSLocalizedString("Title: \(indexPath.row)", comment: "Song cell title")
+        cell.detailTextLabel?.text = NSLocalizedString("Subtitle: \(indexPath.row)", comment: "Song cell subtitle")
+        
+        cell.imageView?.image = UIImage(named: "thumb")
+        
+        return cell
+    }
 }
 
